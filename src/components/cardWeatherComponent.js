e118a20715ee37b374fe510cff5aae548acb57a4
Yana Seniuk <senyukyana26@gmail.com>
2025-04-10 16:50:52 +0200

    add weather API||implement API to weather card

--- src/components/cardWeatherComponent.js
+++ src/components/cardWeatherComponent.js
@@ -1,8 +1,14 @@
-import { WEATHER_LOCATION_ID, WEATHER_TEMP_ID } from "../constants.js";
+import {
+  WEATHER_LOCATION_ID,
+  WEATHER_TEMP_ID,
+  WEATHER_CONDITION_TEXT_ID,
+  WEATHER_CONDITION_PIC_ID,
+  WEATHER_CARD_ID,
+} from "../constants.js";
 
 export function createCardSWeather(weatherData) {
   const cardElement = document.createElement("div");
-  cardElement.className = "weather__card";
+  cardElement.id = `${WEATHER_CARD_ID}`;
   cardElement.innerHTML = `
   <div class="weather__card-date"> 
  <span>${weatherData.day}</span>
@@ -10,14 +16,14 @@ export function createCardSWeather(weatherData) {
  </div>
 <div class="weather__card-body">
 
- <div class="weather__card-content> 
- <div id=${WEATHER_LOCATION_ID}>${weatherData.location.name}</div>
- <div id =${WEATHER_TEMP_ID}>${weatherData.current.temp_c}</div>
- <div id =${WEATHER_CONDITION_TEXT_ID}>${weatherData.current.condition.text}</div>
+ <div class="weather__card-content"> 
+ <div id="${WEATHER_LOCATION_ID}">${weatherData.location.name}</div>
+ <div id ="${WEATHER_TEMP_ID}">${weatherData.current.temp_c}°</div>
+ <div id ="${WEATHER_CONDITION_TEXT_ID}">${weatherData.current.condition.text}</div>
  </div>
 
-<div class="weather__card-pic>
-<img id=${WEATHER_CONDITION_PIC_ID} src='${weatherData.current.condition.icon}'>
+<div class="weather__card-pic">
+<img id="${WEATHER_CONDITION_PIC_ID}" src='${weatherData.current.condition.icon}'>
  </div>
  `;
   return cardElement;
--- src/components/searchWeatherComponent.js
+++ src/components/searchWeatherComponent.js
@@ -1,13 +1,17 @@
-import { CITY_NAME_INPUT_ID, SEARCH_WEATHER_BUTTON_ID } from "../constants.js";
+import {
+  CITY_NAME_INPUT_ID,
+  SEARCH_WEATHER_BUTTON_ID,
+  WEATHER_SEARCH_FORM_ID,
+} from "../constants.js";
 
 export function createSearchWeather() {
   const headerElement = document.createElement("header");
   headerElement.className = "weather__header";
   headerElement.innerHTML = `
  <h1 class="weather__title"></h1> 
- <form class= "weather__search">
+ <form id= ${WEATHER_SEARCH_FORM_ID}>
   <label for="city">Enter the city name:</label>
-  <input type="text" id="${CITY_NAME_INPUT_ID}" name="city" placeholder="Enter the city name">
+  <input type="text" id=${CITY_NAME_INPUT_ID} name="city" placeholder="Enter the city name">
   <button type="submit" id=${SEARCH_WEATHER_BUTTON_ID}>Find</button>
 </form>
   `;
